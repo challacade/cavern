cf5e341fe627af7be2d8e8be1420bee5dcbbabd2
Kyle Schaub <kylejschaub@gmail.com>
2018-03-11 16:00:31 -0500

    New tilesheets for levels

--- source/draw.lua
+++ source/draw.lua
@@ -1,5 +1,7 @@
 local function drawGameplay()
   love.graphics.setLineWidth(2)
+
+  love.graphics.setColor(255, 255, 255, 255)
   mapdata.map:drawLayer(mapdata.map.layers["Main_Tiles"])
 
   enemies:drawHealthBars()
