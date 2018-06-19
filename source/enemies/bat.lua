820657a69a5ec53d0f7d961f0df71a4d0dd11991
Kyle Schaub <kylejschaub@gmail.com>
2018-06-18 21:59:19 -0500

    Flyers now have flapping wings

--- source/enemies/bat.lua
+++ source/enemies/bat.lua
@@ -1,7 +1,7 @@
 local function batInit(enemy, x, y, arg)
 
   -- Initialize physics
-  enemy.physics = world:newBSGRectangleCollider(x, y, 82, 82, 22)
+  enemy.physics = world:newBSGRectangleCollider(x, y, 92, 92, 25)
   enemy.physics:setCollisionClass('Enemy')
   enemy.physics:setLinearDamping(2)
   enemy.physics:setFixedRotation(true)
@@ -11,12 +11,15 @@ local function batInit(enemy, x, y, arg)
   -- Properties
   enemy.health = 10
   enemy.hitPower = 2
-  enemy.moveForce = 4500
+  enemy.moveForce = 6500
   enemy.maxSpeed = 400
   enemy.barY = 62
 
   -- Sprite info
-  enemy.sprite = sprites.enemies.flyer1
+  enemy.sprite = sprites.enemies.flyerBody
+  enemy.wingSprite = sprites.enemies.flyerWing1
+  enemy.spriteTimerBase = 0.001
+  enemy.spriteTimer = enemy.spriteTimerBase
 
   function enemy:update(dt)
     local speed = speedFromVelocity( self.physics:getLinearVelocity() )
@@ -26,14 +29,38 @@ local function batInit(enemy, x, y, arg)
       dir = dir * self.moveForce
       self.physics:applyForce(dir:unpack())
     end
+
+    -- Update the animation; alternate between the two frames
+    self.spriteTimer = updateTimer(self.spriteTimer, dt)
+    if self.spriteTimer == 0 then
+      if self.wingSprite == sprites.enemies.flyerWing1 then
+        self.wingSprite = sprites.enemies.flyerWing2
+      else
+        self.wingSprite = sprites.enemies.flyerWing1
+      end
+      self.spriteTimer = self.spriteTimerBase
+    end
   end
 
   function enemy:draw()
-    love.graphics.setColor(255, 255, 255, 255)
-
-    local sprW = self.sprite:getWidth()
-    local sprH = self.sprite:getHeight()
+    local sprW = self.wingSprite:getWidth()
+    local sprH = self.wingSprite:getHeight()
     local sprX, sprY = self.physics.body:getPosition()
+
+    local wingOffset = -24
+    if self.wingSprite == sprites.enemies.flyerWing2 then
+      wingOffset = wingOffset + 5
+    end
+
+    -- Draw the wings first
+    love.graphics.setColor(255, 255, 255, 80)
+    love.graphics.draw(self.wingSprite, sprX, sprY + wingOffset, nil, 1, 1, sprW/2, sprH/2)
+
+    sprW = self.sprite:getWidth()
+    sprH = self.sprite:getHeight()
+
+    -- Draw the body of the flyer
+    love.graphics.setColor(255, 255, 255, 255)
     love.graphics.draw(self.sprite, sprX, sprY, nil, 1, 1, sprW/2, sprH/2)
   end
 
--- source/startup/loadSprites.lua
+++ source/startup/loadSprites.lua
@@ -14,7 +14,9 @@ sprites.environment.rockySurface = love.graphics.newImage('sprites/environment/r
 
 -- Images for enemies
 sprites.enemies = {}
-sprites.enemies.flyer1 = love.graphics.newImage('sprites/enemies/flyer1.png')
+sprites.enemies.flyerBody = love.graphics.newImage('sprites/enemies/flyerBody.png')
+sprites.enemies.flyerWing1 = love.graphics.newImage('sprites/enemies/flyerWing1.png')
+sprites.enemies.flyerWing2 = love.graphics.newImage('sprites/enemies/flyerWing2.png')
 
 -- Individual blob shapes for drawing fire
 sprites.fire = {}
